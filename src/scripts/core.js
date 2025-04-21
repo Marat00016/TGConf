282a83b80f69026090b6e30d4ef41887fe0114b7
Азат <azat@MacBook-Pro-Azat.local>
2025-04-21 13:56:45 +0300

    Доделал частично адаптив кроме спикеров

--- src/scripts/core.js
+++ src/scripts/core.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
   initAnchors();
   loopTape();
   initHeaderAnimation();
-  connectMap();
   initFutureSlider();
   initTickets();
+  connectMap();
 })
